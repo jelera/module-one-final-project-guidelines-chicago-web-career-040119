a378fd16a400b7b7e80c400efcf90ee76f4baecf
Jose Elera <jelera@gmail.com>
2019-04-17 16:07:21 -0500

    implemented the destinations view

--- views/destinations.rb
+++ views/destinations.rb
@@ -1,10 +1,46 @@
 require 'tty-prompt'
-require_relative 'modules/banner.rb'
+require_relative 'modules/banner'
+require_relative 'modules/helpers'
 
 class Destination
   include Banner
+  include Helper
 
   def initialize
+    @prompt = TTY::Prompt.new
+    destinations_banner
+    tasks
+  end
+
+  def options
+    @choice = @prompt.select("These are the options:") do |menu|
+      menu.default 1
+
+      menu.choice 'Get destinations by city', 1
+      menu.choice 'Read reviews about a destination', 2
+      menu.choice 'Get a destination detailed info', 3
+      menu.choice 'List all Destinations', 4
+      menu.choice 'Go back to the main screen', 5
+      menu.choice 'Exit Program', 6
+    end
+  end
+
+  def tasks
+    case options
+    when 1
+      puts "all destinations by city"
+    when 2
+      ""
+    when 3
+      ""
+    when 4
+      ""
+    when 5
+      go_back_to_main_screen
+    when 6
+      system 'clear'
+      exit
+    end
   end
 
 end
