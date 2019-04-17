484b17cbb18cd435b6503fdfd2b8094c97fffc2e
Jose Elera <jelera@gmail.com>
2019-04-17 14:54:38 -0500

    Refactor the book_flight view

--- views/book_flight.rb
+++ views/book_flight.rb
@@ -1,8 +1,41 @@
 require 'tty-prompt'
+require_relative 'modules/banner.rb'
+
 class BookFlight
+  attr_reader :choice
+  include Banner
 
+  def initialize
+    @prompt = TTY::Prompt.new
+    spacer_banner
+    book_flight_banner
+    options
+    confirmation
+  end
+
+  def options
+    @choice = @prompt.collect do
+
+      key(:departure_city).ask("Enter your departure city", required: true, default: 'Chicago') do |q|
+      # q.validate
+      end
+
+      key(:arrival_city).ask("Enter your arrival city", required: true, default: 'New York City') do |q|
+      # q.validate
+      end
+    end
+  end
+
+  def confirmation
+    puts <<-MSG
+
+
+Here are the details for your upcoming flight
+
+Departure city: #{@choice[:departure_city]}
+Arrival city: #{@choice[:arrival_city]}
+
+
+    MSG
+  end
 end
-
-
-
-
