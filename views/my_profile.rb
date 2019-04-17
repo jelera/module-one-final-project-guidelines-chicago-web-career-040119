bfe04a0714bb4f31ac79b6b45f9e33decfb7d0b2
Sarah Elzinga <sevans924@gmail.com>
2019-04-17 16:25:49 -0500

    added return to main menu prompts

--- views/my_profile.rb
+++ views/my_profile.rb
@@ -19,6 +19,8 @@ class MyProfile
       menu.choice 'My Info', 1
       menu.choice 'My Flights', 2
       menu.choice 'My Reviews', 3
+      menu.choice 'Go back to the main screen', 4
+      menu.choice 'Exit Program', 5
       # menu.choice 'Return to Main Menu', 4
     end
   end
@@ -33,6 +35,8 @@ class MyProfile
         menu.choice 'Change Password', 2
         menu.choice 'Change Name', 3
         menu.choice 'Change Address', 4
+        menu.choice 'Go back to My Profile menu', 5
+
       end
 
     when 2
@@ -43,6 +47,7 @@ class MyProfile
         menu.choice 'Past Flights', 2
         menu.choice 'Upcoming Flights', 3
         menu.choice 'Update My Flights', 4
+        menu.choice 'Go back to My Profile menu', 5
       end
     when 3
       @prompt.select("My Reviews") do |menu|
@@ -51,6 +56,7 @@ class MyProfile
         menu.choice 'Write a New Review', 1
         menu.choice 'Read My Reviews', 2
         menu.choice 'Update an Existing Review', 3
+        menu.choice 'Go back to My Profile menu', 4
       end
     end
 
