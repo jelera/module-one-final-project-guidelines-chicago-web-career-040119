f2bf847463cf730d77a8f4531eeaaf289b61fb9f
Jose Elera <jelera@gmail.com>
2019-04-18 16:32:20 -0500

    book flight sorta works

--- lib/views/book_flight.rb
+++ lib/views/book_flight.rb
@@ -9,6 +9,7 @@ class BookFlightView
     spacer_banner
     book_flight_banner
     options
+    validate_cities
     confirmation
   end
 
@@ -25,14 +26,50 @@ class BookFlightView
     end
   end
 
+  def validate_cities
+    @d_city = City.where("name=?", @choice[:departure_city]).first
+    @a_city = City.where("name=?", @choice[:arrival_city]).first
+
+    @flights = Flight.where('departure_city_id=? and arrival_city_id=?', @d_city.id, @a_city.id)
+
+    if @flights.empty?
+      puts "Sorry, there are no flights to those cities"
+    else
+      print_flights(@flights)
+    end
+  end
+
+
+  def print_flights(flights)
+
+    puts "Here are the available flights"
+    puts ""
+    puts ""
+    puts ""
+    flights.each_with_index do |flight, index|
+      # binding.pry
+      puts "SELECTION #{index + 1}"
+      puts "-----------------------------------------------"
+      puts "Flight number: " + flight[:flight_number]
+      puts "Airline: " + flight[:airline]
+      puts "Price: " + flight[:price].to_s
+      puts "-----------------------------------------------"
+      puts ""
+    end
+
+  end
+
   def confirmation
+    selection = @prompt.ask('Which flight would you like? Enter the Selection number')
+    selected_flight = @flights[selection.to_i - 1]
+    UserFlight.create(user_id: $user.id, flight_id: selected_flight.id)
     puts <<-MSG
 
 
 Here are the details for your upcoming flight
 
-Departure city: #{@choice[:departure_city]}
-Arrival city: #{@choice[:arrival_city]}
+Departure city: #{@d_city.name}
+Arrival city: #{@a_city.name}
 
 
     MSG
