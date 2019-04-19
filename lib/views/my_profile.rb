824b8a5ba09303fa83c52d5ec69967322f30b5ee
Sarah Elzinga <sevans924@gmail.com>
2019-04-18 20:44:01 -0500

    includes the latest methods in my profile - view my flights might not be functional at the moment

--- lib/views/my_profile.rb
+++ lib/views/my_profile.rb
@@ -41,7 +41,44 @@ def my_profile_tasks
         puts profile.email
         puts profile.password
       when 2
-        #Change profile
+        user = @prompt.collect do
+
+          key(:age).ask('What is your age (1-150)? ') do |q|
+            q.in '18-150'
+            q.messages[:range?] = '%{value} is not quite old enough to create a profile. Please go get an adult.'
+          end
+
+          key(:username).ask('E-mail Address(this will be your username):') do |q|
+          q.validate(/\A\w+@\w+\.\w+\Z/)
+          q.messages[:valid?] = 'Invalid email address'
+          end
+
+          key(:password).ask('Create a password:')
+
+          key(:first_name).ask('First Name')
+
+          key(:last_name).ask('Last Name')
+
+          key(:address) do
+            key(:street).ask('Street?', required: true)
+            key(:city).ask('City?')
+            key(:state).ask('State?')
+            key(:zip).ask('Zip?', validate: /\A\d{3}\Z/)
+          end
+
+          key(:country_origin).ask('Country of Origin: ')
+
+        end
+        $user = User.update(
+          first_name: user[:first_name],
+          last_name: user[:last_name],
+          age: user[:age],
+          address: "#{user[:street]}, #{user[:city]}, #{user[:state]} #{user[:zip]}",
+          email: user[:username],
+          password: user[:password],
+          country_origin: user[:country_origin]
+        )
+        puts my_profile_main_menu
       when 3
 
        $user.reviews.each do |r|
